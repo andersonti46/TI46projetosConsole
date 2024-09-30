5de668e2d09b0758bad66ffb49f7d86826cf7a02
ANDERSON FERNANDES DE LIMA <anderson.flima7@senacsp.edu.br>
2024-09-30 20:17:16 -0300

    primeira versão do projeto TI48ProjetosConsole

---
+++ TI46ProjetosConsole2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TI46ProjetosConsole2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
